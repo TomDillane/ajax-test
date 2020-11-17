085d640bce187208852ef12874d19ebc53299422
TomDillane <tommy.c.dillane@gmail.com>
2020-11-17 22:40:35 +0000

    Doc: updated JS file with api

--- main.js
+++ main.js
@@ -1,11 +1,48 @@
+const baseURL = "https://ci-swapi.herokuapp.com/api/";
+
+function getData(type, cb) {
 var xhr = new XMLHttpRequest();
+
+
+xhr.open("GET", baseURL + type + "/");
+xhr.send();
+
+function setData(jsonData) {
+    data = jsonData;
+    
+}
 
 xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
-    }
-};
+        cb(JSON.parse(this.responseText));
+        }
+    };
+}
 
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+function getTableHeaders(obj) {
+    var tableHeaders = [];
 
-xhr.send();
+    Object.keys(obj).forEach(function(key) {
+        tableHeaders.push(`<td>${key}</td>`);
+    });
+    
+    return `<tr>${tableHeaders}</tr>`;
+}
+
+function writeToDocument(type) {
+    var el = document.getElementById("data");
+    el.innerHTML = "";
+    getData(type, function(data) {
+        data = data.results;
+        var tableHeaders = getTableHeaders(data[0]);
+
+        data.forEach(function(item) {
+            Object.keys(item).forEach(function(key){
+                console.log(key);
+            })
+       // el.innerHTML += "<p>" + item.name + "</p>";
+        });      
+        
+        el.innerHTML = `<table>${tableHeaders}</table>`;
+    });
+}
