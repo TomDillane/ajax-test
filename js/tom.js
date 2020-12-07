9b61b4ed093fd042f8744010959f8ef17b01c37f
TomDillane <tommy.c.dillane@gmail.com>
2020-12-07 21:31:15 +0000

    Table from JSON practise

--- js/tom.js
+++ js/tom.js
@@ -1,16 +1,61 @@
-let xhr = new XMLHttpRequest();
-let data;
+const baseURL = "https://ci-swapi.herokuapp.com/api/";
 
-xhr.open('Get', 'https://ci-swapi.herokuapp.com/api/');
-xhr.send();
+function getData(type, cb) {
+    var xhr = new XMLHttpRequest();
 
-xhr.onreadystatechange = function() {
-    console.log(this.readyState);
-    if (this.readyState == 4 && this.status == 200) {
-        data = JSON.parse(this.responseText);
-    }
-};
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
 
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
+}
+
+function getTableHeaders(obj) {
+    let tableHeaders = [];
+
+    Object.keys(obj).forEach(function(key) {
+        tableHeaders.push(`<td>${key}</td>`)
+    });
+    return `<tr>${tableHeaders}</tr>`;
+}
+
+function writeToDocument(type) {
+    let tableRows = [];
+    let el = document.getElementById('tomd');
+    el.innerHTML = "";
+    getData(type, function(data) {
+       // console.dir(data);
+        data = data.results;
+        let tableHeaders = getTableHeaders(data[0]);
+        data.forEach(function(item) {
+            let dataRow = [];
+            Object.keys(item).forEach(function(key) {
+                dataRow.push(`<td>${item[key]}</td>`);
+            });
+            tableRows.push(dataRow);
+            
+          //  el.innerHTML += "<p>" + item.name + "</p>";
+        });
+        
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+    });
+}
+
+
+/*function printDataToConsole(data) {
+    document.getElementById('tomd').innerHTML = JSON.stringify(data);
+    console.log(data);
+}
+
+getData(printDataToConsole);
+*/
+
+
+/*
 setTimeout(function() {
-    document.getElementById('tomd').innerHTML = data;
+    document.getElementById('tomd').innerHTML = JSON.stringify(data);
 }, 500);
+*/
